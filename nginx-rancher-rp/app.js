b69b58825623a2805bdd7abb4792d9b9e05df830
Olivier ORABONA <olivier.orabona@gmail.com>
2016-09-03 16:13:20 +0200

    Added URL cleaning

--- nginx-rancher-rp/app.js
+++ nginx-rancher-rp/app.js
@@ -5,59 +5,60 @@ var util = require("util");
 var Q = require("q");
 var later = require("later");
 var exec = require('promised-exec');
+var URL = require('url');
 
 var readFile = Q.nfbind(fs.readFile);
 var writeFile = Q.nfbind(fs.writeFile);
 
 // Retrieve environment variables
 var env = process.env;
-var url = env.RANCHER_METADATA_HOST + "/" + env.RANCHER_VERSION + "/containers";
+var rawUrl = env.RANCHER_METADATA_HOST + "/" + env.RANCHER_VERSION + "/containers";
+var url = rawUrl.replace(/\//g,"");
 
 console.log("Gathering containers data from Rancher API: "+url);
 var templateVhost = fs.readFileSync("nginx-default-vhost.conf");
 
 var api = function(url) {
-	console.log("get "+url);
-	var opts = {
-	  uri: url,
-	  method: "GET"
-	};
-	return Q.nfcall(request, opts).spread(function(res,body) {
-	  console.log("Got status response: " + res.statusCode);
-	  console.log("Parsing results... ");
-	  return JSON.parse(body);
-	});
+  console.log("get "+url);
+  var opts = {
+    uri: url,
+    method: "GET"
+  };
+  return Q.nfcall(request, opts).spread(function(res,body) {
+    console.log("Got status response: " + res.statusCode);
+    console.log("Parsing results... ");
+    return JSON.parse(body);
+  });
 }
 
 var main = function() {
-api(url)
-.then(function(containers) {
-	var data = containers.data;
-	var currentVhostFile = "";
-	data.forEach(function(el,idx) {
-		var env = el.environment;
-		var containerName = el.name;
-		var remoteAddress = el.data.fields.dockerHostIp;
-		console.log("Container "+idx+" environment: "+JSON.stringify(env));
-		if(env && env.VIRTUAL_HOST) {
-			var virtualPort = env.VIRTUAL_PORT || 80;
-			currentVhostFile += "upstream "+env.VIRTUAL_HOST+" {\n\tserver "+remoteAddress+":"+virtualPort+";\n}\n";
-			console.log("Adding "+containerName+" ("+remoteAddress+") vhost to Nginx...");
-		}
-	});
-	return currentVhostFile;
-})
-.then(function(upstream) {
-	var currentVhostFile = upstream+"\n"+templateVhost;
-	fs.writeFileSync("/etc/nginx/conf.d/default.conf", currentVhostFile);
-//	process.exit(0);
-})
-.then(function() {
-	return exec("nginx -s reload")
-})
-.catch(function(e) {
-  console.log("Got error : " + util.inspect(e));
-});
+  api(url)
+  .then(function(containers) {
+    var data = containers.data;
+    var currentVhostFile = "";
+    data.forEach(function(el,idx) {
+      var env = el.environment;
+      var containerName = el.name;
+      var remoteAddress = el.data.fields.dockerHostIp;
+      console.log("Container "+idx+" environment: "+JSON.stringify(env));
+      if(env && env.VIRTUAL_HOST) {
+        var virtualPort = env.VIRTUAL_PORT || 80;
+        currentVhostFile += "upstream "+env.VIRTUAL_HOST+" {\n\tserver "+remoteAddress+":"+virtualPort+";\n}\n";
+        console.log("Adding "+containerName+" ("+remoteAddress+") vhost to Nginx...");
+      }
+    });
+    return currentVhostFile;
+  })
+  .then(function(upstream) {
+    var currentVhostFile = upstream+"\n"+templateVhost;
+    return fs.writeFile("/etc/nginx/conf.d/default.conf", currentVhostFile);
+  })
+  .then(function() {
+    return exec("nginx -s reload")
+  })
+  .catch(function(e) {
+    console.log("Got error : " + util.inspect(e));
+  });
 }
 
 var cron = env.CRON || "every 30 sec"
